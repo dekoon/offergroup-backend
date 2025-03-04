be907185acf8a215fa8329604d6f95cd5c43e344
dekoon <105488793+dekoon@users.noreply.github.com>
2025-03-05 07:03:12 +0900

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -7,4 +7,6 @@ router.get("/pays/:userId", paymentController.payMultiItems);
 router.post("/pays/:userId", paymentController.saveMultiOrder);
 router.post("/stockStatus0/:userId", paymentController.updateStockStatusToZero);
 router.post("/stockStatus2/:userId", paymentController.updateStockStatusToTwo);
+//
+router.post("/updatepgorder", paymentController.updateOrder);
 module.exports = router;
